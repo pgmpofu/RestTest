3dc96d34e6b191d1af50f00bfd31bd569cad4585
Gugu Mpofu <pgmpofu@gmail.com>
2022-08-20 15:17:21 -0700

    Configure data source and add E2E implementation of api to list all transactions

--- src/main/java/com/bench/resttest/service/RestTestTransactionService.java
+++ src/main/java/com/bench/resttest/service/RestTestTransactionService.java
@@ -5,10 +5,6 @@ import com.bench.resttest.repository.RestTestTransactionRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-
-import java.util.Iterator;
-import java.util.List;
-
 @Service
 public class RestTestTransactionService {
 
